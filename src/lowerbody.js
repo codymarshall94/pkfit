5f5112f50941c3d7314c38dd492a9133c9f6e686
Cody Marshall <codymstuntsx@gmail.com>
2022-08-12 15:54:24 -0700

    added more exercises

--- src/lowerbody.js
+++ src/lowerbody.js
@@ -1,7 +1,7 @@
 export const LOWERBODY = [
     {
         id: 1,
-        name: "Step up",
+        name: "Box Step up",
     },
     {
         id: 2,
@@ -14,5 +14,29 @@ export const LOWERBODY = [
     {
         id: 4,
         name: "1 Leg box jump",
-    }
+    },
+    {
+        id: 5,
+        name: "Wall sit",
+    },
+    {
+        id: 6,
+        name: "Lunge",
+    },
+    {
+        id: 7,
+        name: "Pogo jump",
+    },
+    {
+        id: 8,
+        name: "1 Leg wall sit",
+    },
+    {
+        id: 9,
+        name: "Squat",
+    },
+    {
+        id: 10,
+        name: "Side to side bound",
+    },
 ]
