bf7d79595a5b9f86e4c06e5995ea98bb41fc4b34
Cody Marshall <codymstuntsx@gmail.com>
2022-08-12 13:11:31 -0700

    starter json data

---
+++ src/lowerbody.js
@@ -0,0 +1,18 @@
+export const LOWERBODY = [
+    {
+        id: 1,
+        name: "Step up",
+    },
+    {
+        id: 2,
+        name: "Box jump",
+    },
+    {
+        id: 3,
+        name: "Broad jump",
+    },
+    {
+        id: 4,
+        name: "1 Leg box jump",
+    }
+]
