922b249b001ff4249abf1a01f4902ad6ee7c16a3
Cody Marshall <codymstuntsx@gmail.com>
2022-08-12 13:11:19 -0700

    header added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/header.css';
+
+function Header() {
+  return (
+    <div className='header'>
+        <h4 className='header-logo'>pkFit</h4>
+    </div>
+  )
+}
+
+export default Header
