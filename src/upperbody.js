76f1498f198810b90e515a7332137029a6baeea4
Cody Marshall <codymstuntsx@gmail.com>
2022-08-12 15:53:51 -0700

    updated id numbers

---
+++ src/upperbody.js
@@ -0,0 +1,42 @@
+export const UPPERBODY = [
+    {
+        id: 11,
+        name: 'Push up'
+    },
+    {
+        id: 12,
+        name: 'Pull up'
+    },
+    {
+        id: 13,
+        name: 'Dip'
+    },
+    {
+        id: 14,
+        name: 'Kong up'
+    },
+    {
+        id: 15,
+        name: 'Bar hang'
+    },
+    {
+        id: 16,
+        name: 'Cat Hang'
+    },
+    {
+        id: 17,
+        name: 'Front support dip'
+    },
+    {
+        id: 18,
+        name: 'Incline push up',
+    },
+    {
+        id: 19,
+        name: 'Behind the back Dip'
+    },
+    {
+        id: 20,
+        name: 'Horizontal row'
+    }
+]
