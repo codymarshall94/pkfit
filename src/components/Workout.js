99f0f56a2189719395a2806d6090f0e6a5d92dda
Cody Marshall <codymstuntsx@gmail.com>
2022-08-15 12:34:58 -0700

    scrollable window

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,15 +1,43 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import "../css/workout.css";
 
 function Workout({ workout }) {
   if (workout) {
     return (
       <Box sx={{ marginTop: "2rem" }}>
-        <Grid container spacing={2}>
+        <Grid
+          container
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "2rem",
+            height: "15rem",
+            overflowY: "auto",
+          }}
+        >
           {workout.map((exercise) => (
-            <Grid key={exercise.id} item xs={12}>
-              {exercise.name}
+            <Grid key={exercise.id} item xs={11} className="workout-item">
+              <Grid item xs={3} sx={{ display: "flex", alignItems: "center" }}>
+                <img
+                  src={require("../images/placeholderthumb.png")}
+                  className="workout-image"
+                  alt=""
+                />
+              </Grid>
+              <Grid
+                item
+                xs={6}
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  paddingLeft: ".25rem",
+                }}
+              >
+                <span>{exercise.name}</span>
+              </Grid>
             </Grid>
           ))}
         </Grid>
