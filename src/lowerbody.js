87ddcd2145e68a96bb78aa8579ef1f2a6295ca63
Cody Marshall <codymstuntsx@gmail.com>
2022-08-15 16:34:47 -0700

    descriptions added

--- src/lowerbody.js
+++ src/lowerbody.js
@@ -2,41 +2,51 @@ export const LOWERBODY = [
     {
         id: 1,
         name: "Box Step up",
+        description: "Place foot of first leg on bench. Stand on bench by extending hip and knee of first leg and place foot of second leg on bench. Step down with second leg by flexing hip and knee of first leg. Return to original standing position by placing foot of first leg to floor.",
     },
     {
         id: 2,
         name: "Box jump",
+        description: "Stand in front of the box with your feet shoulder-width apart. Bend into a quarter squat and swing your arms back, then swing them forward and explode up off the ground. Land on the box as softly as possible.",
     },
     {
         id: 3,
         name: "Broad jump",
+        description: "Stand shoulder width apart with your arms in the air. Swing arms back as you bend at the knees. Swings arms and jumps forwards, landing on the ground",
     },
     {
         id: 4,
         name: "1 Leg box jump",
+        description: "Quickly jump on and off a low platform on one leg. Be sure to land on the toes and utilize the stretch-reflex.",
     },
     {
         id: 5,
         name: "Wall sit",
+        description: "Place back against wall and lower down untill knees form a 90degree angle",
     },
     {
         id: 6,
         name: "Lunge",
+        description: "Step forward and place foot on ground, lowering back knee to floor. Push back up to initial position",
     },
     {
         id: 7,
         name: "Pogo jump",
+        description: "Stand shoulder width apart standing on balls of feet. Quickly bounce up and down without letting your heels touch the ground",
     },
     {
         id: 8,
         name: "1 Leg wall sit",
+        description: "Place back against wall and lower down untill knees form a 90degree angle. Lift one leg up and hold",
     },
     {
         id: 9,
         name: "Squat",
+        description: "Squat down by bending hips back while allowing knees to bend forward, keeping back straight and knees pointed same direction as feet. Descend until thighs are just past parallel to floor. Squat up by extending knees and hips until legs are straight. Return and repeat.",
     },
     {
         id: 10,
         name: "Side to side bound",
+        description: "Jump far to right side and land right foot with left foot off of ground. Jump far to left side landing on right foot. Continue to bound from side to side.",
     },
 ]
--- src/upperbody.js
+++ src/upperbody.js
@@ -1,42 +1,62 @@
 export const UPPERBODY = [
     {
         id: 11,
-        name: 'Push up'
+        name: 'Push up',
+        description: 'Keeping body straight, lower body to floor by bending arms. Push body up until arms are extended. Repeat.'
     },
     {
         id: 12,
-        name: 'Pull up'
+        name: 'Pull up',
+        description: 'Pull body up until neck reaches height of hands. Lower body until arms and shoulders are fully extended. Repeat.'
     },
     {
         id: 13,
-        name: 'Dip'
+        name: 'Dip',
+        description: 'Lower body by bending arms, allowing elbows to flare out to sides. When slight stretch is felt in chest or shoulders, push body up until arms are straight. Repeat.'
     },
     {
         id: 14,
-        name: 'Kong up'
+        name: 'Kong up',
+        description: 'Pushing through arms, allow knees to come upwards and land feet on top of object. Ending in squatting or standing position. '
     },
     {
         id: 15,
-        name: 'Bar hang'
+        name: 'Bar hang',
+        description: 'Holding on to the bar keeping arms and body straight'
     },
     {
         id: 16,
-        name: 'Cat Hang'
+        name: 'Cat Hang',
+        description: 'Starting with hands holding on to wall and feet resting on wall. Just a simple hold'
     },
     {
         id: 17,
-        name: 'Front support dip'
+        name: 'Front support dip',
+        description: 'Starting with arms locked out bend at the elbows, lowering the chest down towards object and then pressing back up to arms locked out'
     },
     {
         id: 18,
         name: 'Incline push up',
+        description: 'Keeping body straight, lower chest to edge of box or platform by bending arms. Push body up until arms are extended. Repeat.'
     },
     {
         id: 19,
-        name: 'Behind the back Dip'
+        name: 'Behind the back Dip',
+        description: 'Lower body by bending arms until slight stretch is felt in chest or shoulder, or rear end touches floor. Raise body and repeat.'
     },
     {
         id: 20,
-        name: 'Horizontal row'
-    }
+        name: 'Inverted row',
+        description: 'Keeping body straight, pull body up to bar. Return until arms are extended and shoulders are stretched forward. Repeat.'
+    },
+    {
+        id: 21,
+        name: 'Quadrapedal movement',
+        description: 'A simple crawl performed by keeping the body low to the ground and moving one leg and the opposite hand forward at the same time.'
+    },
+    {
+        id: 22,
+        name: 'Plyo pushup',
+        description: 'Keeping body straight, lower body to floor by bending arms. Push body up rapidly so that hands lose contact with ground'
+    }, 
 ]
