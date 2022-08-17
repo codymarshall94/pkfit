40b1cce8ddc17aceee8d4162080fec8b5a478dc4
Cody Marshall <codymstuntsx@gmail.com>
2022-08-17 13:05:03 -0700

    added new exercises

--- src/upperbody.js
+++ src/upperbody.js
@@ -1,62 +1,74 @@
 export const UPPERBODY = [
     {
-        id: 11,
-        name: 'Push up',
-        description: 'Keeping body straight, lower body to floor by bending arms. Push body up until arms are extended. Repeat.'
-    },
-    {
-        id: 12,
-        name: 'Pull up',
-        description: 'Pull body up until neck reaches height of hands. Lower body until arms and shoulders are fully extended. Repeat.'
-    },
-    {
-        id: 13,
-        name: 'Dip',
-        description: 'Lower body by bending arms, allowing elbows to flare out to sides. When slight stretch is felt in chest or shoulders, push body up until arms are straight. Repeat.'
-    },
-    {
-        id: 14,
-        name: 'Kong up',
-        description: 'Pushing through arms, allow knees to come upwards and land feet on top of object. Ending in squatting or standing position. '
-    },
-    {
-        id: 15,
-        name: 'Bar hang',
-        description: 'Holding on to the bar keeping arms and body straight'
-    },
-    {
         id: 16,
         name: 'Cat Hang',
-        description: 'Starting with hands holding on to wall and feet resting on wall. Just a simple hold'
+        description: 'Starting with hands holding on to wall and feet resting on wall. Just a simple hold',
+        image: null,
     },
     {
         id: 17,
         name: 'Front support dip',
-        description: 'Starting with arms locked out bend at the elbows, lowering the chest down towards object and then pressing back up to arms locked out'
+        description: 'Starting with arms locked out bend at the elbows, lowering the chest down towards object and then pressing back up to arms locked out',
+        image: null,
     },
     {
         id: 18,
         name: 'Incline push up',
-        description: 'Keeping body straight, lower chest to edge of box or platform by bending arms. Push body up until arms are extended. Repeat.'
+        description: 'Keeping body straight, lower chest to edge of box or platform by bending arms. Push body up until arms are extended. Repeat.',
+        image: null,
     },
     {
         id: 19,
         name: 'Behind the back Dip',
-        description: 'Lower body by bending arms until slight stretch is felt in chest or shoulder, or rear end touches floor. Raise body and repeat.'
+        description: 'Lower body by bending arms until slight stretch is felt in chest or shoulder, or rear end touches floor. Raise body and repeat.',
+        image: null,
     },
     {
         id: 20,
         name: 'Inverted row',
-        description: 'Keeping body straight, pull body up to bar. Return until arms are extended and shoulders are stretched forward. Repeat.'
+        description: 'Keeping body straight, pull body up to bar. Return until arms are extended and shoulders are stretched forward. Repeat.',
+        image: null,
     },
     {
         id: 21,
         name: 'Quadrapedal movement',
-        description: 'A simple crawl performed by keeping the body low to the ground and moving one leg and the opposite hand forward at the same time.'
+        description: 'A simple crawl performed by keeping the body low to the ground and moving one leg and the opposite hand forward at the same time.',
+        image: null,
     },
     {
         id: 22,
         name: 'Plyo pushup',
-        description: 'Keeping body straight, lower body to floor by bending arms. Push body up rapidly so that hands lose contact with ground'
+        description: 'Keeping body straight, lower body to floor by bending arms. Push body up rapidly so that hands lose contact with ground',
+        image: null,
+    },
+    {
+        id: 23,
+        name: 'Push up',
+        description: 'Keeping body straight, lower body to floor by bending arms. Push body up until arms are extended. Repeat.',
+        image: null,
+    },
+    {
+        id: 24,
+        name: 'Pull up',
+        description: 'Pull body up until neck reaches height of hands. Lower body until arms and shoulders are fully extended. Repeat.',
+        image: null,
+    },
+    {
+        id: 25,
+        name: 'Dip',
+        description: 'Lower body by bending arms, allowing elbows to flare out to sides. When slight stretch is felt in chest or shoulders, push body up until arms are straight. Repeat.',
+        image: null,
+    },
+    {
+        id: 26,
+        name: 'Kong up',
+        description: 'Pushing through arms, allow knees to come upwards and land feet on top of object. Ending in squatting or standing position. ',
+        image: null,
+    },
+    {
+        id: 27,
+        name: 'Bar hang',
+        description: 'Holding on to the bar keeping arms and body straight',
+        image: null,
     }, 
 ]
