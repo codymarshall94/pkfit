a2b35eef89f2a46ed00377afe3295227897690a7
Cody Marshall <codymstuntsx@gmail.com>
2022-08-17 13:18:38 -0700

    changed reps prop to goal

--- src/components/GenerateBtn.js
+++ src/components/GenerateBtn.js
@@ -2,13 +2,13 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
-function GenerateBtn({ generateWorkout, workoutType, exerciseAmount, reps }) {
+function GenerateBtn({ generateWorkout, workoutType, exerciseAmount, goal }) {
   return (
     <Box sx={{ margin: "1rem" }}>
       <Button
         variant="contained"
         onClick={() => generateWorkout()}
-        disabled={workoutType && exerciseAmount && reps ? false : true}
+        disabled={workoutType && exerciseAmount && goal ? false : true}
       >
         GENERATE
       </Button>
