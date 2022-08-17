b3bbb859251e42de139af8eb3810d5730f9769fa
Cody Marshall <codymstuntsx@gmail.com>
2022-08-17 13:19:14 -0700

    added ternary for image selection

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
 import "../css/workout.css";
 
-function Workout({ workout, openModalWithExercise }) {
-
+function Workout({ workout, openModalWithExercise, sets, reps }) {
   if (workout) {
     return (
       <Box sx={{ marginTop: "2rem" }}>
@@ -20,25 +19,53 @@ function Workout({ workout, openModalWithExercise }) {
           }}
         >
           {workout.map((exercise) => (
-            <Grid key={exercise.id} item xs={11} className="workout-item" onClick={() => openModalWithExercise({exercise})} sx={{margin: '.25rem'}}>
-              <Grid item xs={3} sx={{ display: "flex", alignItems: "center" }}>
-                <img
-                  src={require("../images/placeholderthumb.png")}
-                  className="workout-image"
-                  alt=""
-                />
+            <Grid
+              key={exercise.id}
+              item
+              xs={11}
+              lg={7}
+              className="workout-item"
+              onClick={() => openModalWithExercise({ exercise })}
+              sx={{ margin: ".25rem" }}
+            >
+              <Grid
+                item
+                xs={3}
+                lg={2}
+                className="workout-image-container"
+                sx={{ display: "flex", alignItems: "center" }}
+              >
+                {exercise.image !== null ? (
+                  <img
+                    src={exercise.image}
+                    className="workout-image"
+                    alt=""
+                    height="80px"
+                  />
+                ) : (
+                  <img
+                    src={require("../images/placeholderthumb.png")}
+                    className="workout-image"
+                    alt=""
+                  />
+                )}
               </Grid>
               <Grid
                 item
                 xs={8}
                 sx={{
                   display: "flex",
+                  width: "1rem",
                   justifyContent: "flex-start",
+                  flexDirection: "column",
                   alignItems: "center",
                   paddingLeft: ".25rem",
                 }}
               >
-                <span>{exercise.name}</span>
+                <span className="workout-name-text">{exercise.name}</span>
+                <span className="workout-reps-text">
+                  {sets} x {reps}
+                </span>
               </Grid>
               <Grid item xs={1} sx={{ display: "flex", alignItems: "center" }}>
                 <ArrowCircleRightOutlinedIcon />
