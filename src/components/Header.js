963174e3b1a63e10685ed0977f2dc4c094c6ed59
Cody Marshall <codymstuntsx@gmail.com>
2022-08-18 09:07:11 -0700

    added links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../css/header.css';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className='header'>
-        <h4 className='header-logo'>pkFit</h4>
+        <Link to="/" style={{ textDecoration: 'none' }}><h4 className='header-logo'>pkFit</h4></Link>
+        <Link to="/generator" style={{ textDecoration: 'none' }}><span className='header-link'>Generate</span></Link>
     </div>
   )
 }
