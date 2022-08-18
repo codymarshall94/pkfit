0df04697c330672e1220ebd91b5a424e5614a0be
Cody Marshall <codymstuntsx@gmail.com>
2022-08-18 09:07:53 -0700

    added routing to pages

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Generator from "./components/Generator";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="generator" element={<Generator />} />
+      </Routes>
+    </BrowserRouter>
 );
 
 
