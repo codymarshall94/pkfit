96f53e9653f88d2495a6811c624af551b2f8c449
Cody Marshall <codymstuntsx@gmail.com>
2022-08-23 16:33:10 -0700

    paths added and redux provider

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Generator from "./components/Generator";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
+import Skills from "./components/Skills";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import Exercises from "./components/Exercises";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="generator" element={<Generator />} />
+        <Route path="skills" element={<Skills />} />
+        <Route path="exercises" element={<Exercises />} />
       </Routes>
     </BrowserRouter>
+  </Provider>
 );
-
 
 reportWebVitals();
