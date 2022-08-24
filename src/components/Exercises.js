71fd23e824ea7eb037dd8d21c85daff64595c36b
Cody Marshall <codymstuntsx@gmail.com>
2022-08-23 18:19:01 -0700

    ternary options for displaying descriptions

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,21 +1,56 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Header from './Header';
+import React from "react";
+import Box from "@mui/material/Box";
+import Header from "./Header";
 import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import "../css/skills.css";
-import ExerciseDisplay from './ExerciseDisplay';
+import ExerciseDisplay from "./ExerciseDisplay";
+
+const jumpDesc =
+  "Exercises that help with Precisions, Strides or just overall leg strength";
+const vaultDesc =
+  "Exercises that promote pushing power and improving hip drive";
+const swingDesc =
+  "Exercises that increase grip strength, hip drive and engagement of the core";
+const balanceDesc =
+  "Exercises that work on coordination and stabilization of smaller muscles";
+const climbDesc = "Exercises that increase grip and pulling strength";
 
 function Exercises() {
   //grabbing state from redux store
   const { value } = useSelector((state) => state.skills);
 
   return (
-    <Box>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        margin: "auto",
+      }}
+    >
       <Header />
-      <Link to="/skills" className='back-button'>Back</Link>
+      <Box className="exercise-header">
+        <Link to="/skills" className="back-button">
+          Back
+        </Link>
         <h1>{value}</h1>
-        <ExerciseDisplay />
+        <h4>
+          {value === "Jump"
+            ? jumpDesc
+            : value === "Vault"
+            ? vaultDesc
+            : value === "Swing"
+            ? swingDesc
+            : value === "Balance"
+            ? balanceDesc
+            : value === "Climb"
+            ? climbDesc
+            : "No Description"}
+        </h4>
+      </Box>
+
+      <ExerciseDisplay />
     </Box>
   );
 }
