9c061399bab7a94de4e78ae23d215259522f7304
Cody Marshall <codymstuntsx@gmail.com>
2022-08-24 13:30:49 -0700

    added back button with icon

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Box from "@mui/material/Box";
+import "../css/skills.css";
 import Header from "./Header";
+import ExerciseDisplay from "./ExerciseDisplay";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import "../css/skills.css";
-import ExerciseDisplay from "./ExerciseDisplay";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const jumpDesc =
   "Exercises that help with Precisions, Strides or just overall leg strength";
@@ -31,8 +33,10 @@ function Exercises() {
     >
       <Header />
       <Box className="exercise-header">
-        <Link to="/skills" className="back-button">
-          Back
+        <Link to="/skills" style={{ textDecoration: 'none' }}>
+          <Button variant="outlined" startIcon={<ArrowBackIcon />}>
+            Back
+          </Button>
         </Link>
         <h1>{value}</h1>
         <h4>
