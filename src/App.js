4261c20aa3dcdbfd210fb2ff80bf8bd999dbca43
Cody Marshall <codymstuntsx@gmail.com>
2022-08-24 12:06:06 -0700

    refactored to home component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
-import Box from "@mui/system/Box";
 import "./App.css";
-import { Link } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Box sx={{ bgcolor: "#F3F3F4", height: "50px" }}></Box>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "flex-start",
-          flexDirection: "column",
-          padding: "1rem",
-        }}
-      >
-        <h1 className="home-title">Parkour Workout Generator</h1>
-        <h3>Quick workouts for anywhere you are</h3>
-      </Box>
-      <Box className="home-image-container">
-        <img
-          className="home-image"
-          src={require("./images/homePageImage.jpg")}
-          alt=""
-        />
-      </Box>
-      <Link
-        to="/generator"
-        className="home-button"
-        style={{ textDecoration: "none" }}
-      >
-        Generate Now
-      </Link>
+      <Home />
     </div>
   );
 }
