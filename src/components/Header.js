3351a13f97845c1a3c9ec8ce6758de9dbbd368b1
Cody Marshall <codymstuntsx@gmail.com>
2022-08-23 18:18:06 -0700

    changed div to Box

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../css/header.css';
 import { Link } from "react-router-dom";
+import Box from '@mui/material/Box';
 
 function Header() {
   return (
-    <div className='header'>
+    <Box className='header'>
         <Link to="/" style={{ textDecoration: 'none' }}><h4 className='header-logo'>pkFit</h4></Link>
         <Link to="/generator" style={{ textDecoration: 'none' }}><span className='header-link'>Generate</span></Link>
         <Link to="/skills" style={{ textDecoration: 'none' }}><span className='header-link'>Skills</span></Link>
-    </div>
+    </Box>
   )
 }
 
