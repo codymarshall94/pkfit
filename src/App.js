190a5a5fe2b1bfa336c9851f0d38d9398819b75f
Cody Marshall <codymstuntsx@gmail.com>
2022-08-23 18:19:31 -0700

    formatting

--- src/App.js
+++ src/App.js
@@ -4,39 +4,37 @@ import Box from "@mui/system/Box";
 import "./App.css";
 import { Link } from "react-router-dom";
 
-
 function App() {
   return (
-
-      <div className="App">
-        <Header />
-        <Box sx={{ bgcolor: "#F3F3F4", height: "50px" }}></Box>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "flex-start",
-            flexDirection: "column",
-            padding: "1rem",
-          }}
-        >
-          <h1 className="home-title">Parkour Workout Generator</h1>
-          <h3>Quick workouts for anywhere you are</h3>
-        </Box>
-        <Box className="home-image-container">
-          <img
-            className="home-image"
-            src={require("./images/homePageImage.jpg")}
-            alt=""
-          />
-        </Box>
-        <Link
-          to="/generator"
-          className="home-button"
-          style={{ textDecoration: "none" }}
-        >
-          Generate Now
-        </Link>
-      </div>
+    <div className="App">
+      <Header />
+      <Box sx={{ bgcolor: "#F3F3F4", height: "50px" }}></Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-start",
+          flexDirection: "column",
+          padding: "1rem",
+        }}
+      >
+        <h1 className="home-title">Parkour Workout Generator</h1>
+        <h3>Quick workouts for anywhere you are</h3>
+      </Box>
+      <Box className="home-image-container">
+        <img
+          className="home-image"
+          src={require("./images/homePageImage.jpg")}
+          alt=""
+        />
+      </Box>
+      <Link
+        to="/generator"
+        className="home-button"
+        style={{ textDecoration: "none" }}
+      >
+        Generate Now
+      </Link>
+    </div>
   );
 }
 
