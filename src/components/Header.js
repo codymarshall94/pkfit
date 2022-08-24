b2013a2602c5e8fe58ef48b635a1bec042e26e31
Cody Marshall <codymstuntsx@gmail.com>
2022-08-24 13:31:11 -0700

    added container around links for styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import '../css/header.css';
+import React from "react";
+import "../css/header.css";
 import { Link } from "react-router-dom";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 
 function Header() {
   return (
-    <Box className='header'>
-        <Link to="/" style={{ textDecoration: 'none' }}><h4 className='header-logo'>pkFit</h4></Link>
-        <Link to="/generator" style={{ textDecoration: 'none' }}><span className='header-link'>Generate</span></Link>
-        <Link to="/skills" style={{ textDecoration: 'none' }}><span className='header-link'>Skills</span></Link>
+    <Box className="header">
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <h4 className="header-logo">pkFit</h4>
+      </Link>
+      <Box >
+        <Link to="/skills" style={{ textDecoration: "none" }} className="header-links">
+          <span className="header-link">Skills</span>
+        </Link>
+        <Link to="/generator" style={{ textDecoration: "none" }} className="header-links">
+          <span className="header-link">Generate</span>
+        </Link>
+      </Box>
     </Box>
-  )
+  );
 }
 
-export default Header
+export default Header;
