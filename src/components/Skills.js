81426e033f82813d2bd80bd0f0ac37ddcb584347
Cody Marshall <codymstuntsx@gmail.com>
2022-08-30 16:24:15 -0700

    added text heading for page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,6 @@
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import React from "react";
 import Header from "./Header";
 import { Link } from "react-router-dom";
@@ -19,12 +20,39 @@ function Skills() {
   const dispatch = useDispatch();
 
   return (
-    <Box sx={{height: "100vh", overflow: "hidden"}}>
+    <Box sx={{ height: "100vh", overflow: "hidden" }}>
       <Header />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: { sm: "0", lg: "16rem" },
+          marginBottom: { sm: "0", lg: "-24rem" },
+        }}
+      >
+        <Typography
+          variant="h2"
+          component="h3"
+          sx={{
+            fontSize: { xs: "1.75rem", lg: "2rem" },
+            fontWeight: "bold",
+            textAlign: "center",
+            padding: "1rem"
+          }}
+        >
+          Exercises for all fundamental skills to get you moving
+        </Typography>
+      </Box>
       <Grid
         container
         spacing={1}
-        sx={{ display: "flex", justifyContent: "center", padding: "1rem", height: "100%", alignItems: "center" }}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "1rem",
+          height: { xs: "60%", lg: "100%" },
+          alignItems: "center",
+        }}
       >
         {skills.map((skill, index) => (
           <Grid item xs={6} md={4} lg={2} key={index}>
@@ -34,7 +62,6 @@ function Skills() {
                 onClick={() => dispatch(changeSkill(skill.name))}
                 sx={{
                   position: "relative",
-                  height: "12rem",
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
