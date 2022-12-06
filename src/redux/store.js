f04a47ad38476b43b6844244a2b988639b2b92e6
Cody Marshall <codymstuntsx@gmail.com>
2022-12-06 14:10:10 -0800

    user slice added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import skillReducer from "./skills";
 import modalReducer from "./modal";
+import userReducer from "./reducers/userSlice";
 
 export const store = configureStore({
   reducer: {
     skills: skillReducer,
     modal: modalReducer,
+    user: userReducer,
   },
 })
