cbfd2fac68739d0626f6525d17fa3369f94275fe
Cody Marshall <codymstuntsx@gmail.com>
2022-12-06 11:42:22 -0800

    removed routes

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Generator from "./components/Generator";
-import reportWebVitals from "./reportWebVitals";
-import Skills from "./components/Skills";
-
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import Exercises from "./components/Exercises";
-
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
@@ -37,16 +31,9 @@ root.render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="generator" element={<Generator />} />
-          <Route path="skills" element={<Skills />} />
-          <Route path="exercises" element={<Exercises />} />
-          <Route path="*" element={<App />} />
-        </Routes>
+        <App />
       </BrowserRouter>
     </Provider>
   </ThemeProvider>
 );
 
-reportWebVitals();
