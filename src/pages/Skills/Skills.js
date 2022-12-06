466f1c3691150ff24090a7114f773e16c370b64c
Cody Marshall <codymstuntsx@gmail.com>
2022-12-06 11:41:45 -0800

    moved into pages folder

---
+++ src/pages/Skills/Skills.js
@@ -0,0 +1,85 @@
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../css/skills.css";
+import { useDispatch } from "react-redux";
+import { changeSkill } from "../../redux/skills";
+
+const skills = [
+  { name: "Vault", image: require("../../images/vault.jpg") },
+  { name: "Swing", image: require("../../images/swing.jpg") },
+  { name: "Balance", image: require("../../images/balance.jpg") },
+  { name: "Jump", image: require("../../images/jumping.jpg") },
+  { name: "Climb", image: require("../../images/climbing.jpg") },
+];
+
+function Skills() {
+  const dispatch = useDispatch();
+
+  return (
+    <Box sx={{ height: "100vh", overflow: "hidden" }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: { sm: "0", lg: "16rem" },
+          marginBottom: { sm: "0", lg: "-24rem" },
+        }}
+      >
+        <Typography
+          variant="h2"
+          component="h3"
+          sx={{
+            fontSize: { xs: "1.25rem", lg: "2rem" },
+            fontWeight: "bold",
+            textAlign: "center",
+            padding: "1rem"
+          }}
+        >
+          Exercises for all fundamental skills to get you moving
+        </Typography>
+      </Box>
+      <Grid
+        container
+        spacing={1}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "1rem",
+          height: { xs: "60%", lg: "100%" },
+          alignItems: "center",
+        }}
+      >
+        {skills.map((skill, index) => (
+          <Grid item xs={6} md={4} lg={2} key={index}>
+            <Link to={`/exercises`}>
+              <Box
+                className="skill-card"
+                onClick={() => dispatch(changeSkill(skill.name))}
+                sx={{
+                  position: "relative",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  textDecoration: "none",
+                  height: {xs: "8rem", sm: "10rem", md: "12rem", lg: "14rem"},
+                }}
+              >
+                <img
+                  className="skill-category-image"
+                  src={skill.image}
+                  alt=""
+                />
+                <span className="skill-category-text">{skill.name}</span>
+              </Box>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+}
+
+export default Skills;
