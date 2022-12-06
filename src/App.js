105253d7695f2411cdf8085aade898538f9fee23
Cody Marshall <codymstuntsx@gmail.com>
2022-12-06 11:42:41 -0800

    added routes for pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./App.css";
-import Home from "./components/Home";
-import Header from "./components/Header";
+import Home from "./pages/Home/Home";
+import Header from "./components/Navbar";
+import Generator from "./pages/Generator/Generator";
+import Exercises from "./components/Exercises";
+import Skills from "./pages/Skills/Skills";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="generator" element={<Generator />} />
+          <Route path="skills" element={<Skills />} />
+          <Route path="exercises" element={<Exercises />} />
+          <Route path="*" element={<App />} />
+        </Routes>
     </div>
   );
 }
