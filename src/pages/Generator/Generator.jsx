d03e65d2f583e413137fd758709b36a2b14f984d
Cody Marshall <codymstuntsx@gmail.com>
2022-12-12 16:49:29 -0800

    refactored code to be more readable

--- src/pages/Generator/Generator.jsx
+++ src/pages/Generator/Generator.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import InfoIcon from "@mui/icons-material/Info";
+import { useSelector } from "react-redux";
 import { EXERCISES } from "../../exercises";
 import ExerciseSelector from "../../components/ExerciseSelector";
 import GenerateBtn from "../../components/GenerateBtn";
@@ -9,6 +10,7 @@ import Workout from "../../components/Workout";
 import DescriptionModal from "../../components/DescriptionModal";
 import HowToModal from "../../components/HowToModal";
 import SaveWorkoutModal from "../../components/SaveWorkoutModal";
+import { Link } from "react-router-dom";
 
 const filteredUpper = EXERCISES.filter((exer) =>
   exer.exerciseType.includes("Upperbody")
@@ -18,14 +20,16 @@ const filteredLower = EXERCISES.filter((exer) =>
 );
 
 function Generator() {
+  const user = useSelector((state) => state.user.user);
   const [workoutType, setWorkoutType] = useState(null);
-  const [exerciseAmount, setExerciseAmount] = useState(null);
+  const [exerciseTime, setExerciseTime] = useState(null);
   const [goal, setGoal] = useState(null);
   const [reps, setReps] = useState(null);
   const [sets, setSets] = useState(null);
+  const [exerciseAmount, setExerciseAmount] = useState(null);
   const [workout, setWorkout] = useState(null);
   const [open, setOpen] = useState(false);
-  const [openHowTo, setOpenHowTo] = useState(false);
+  const [openInfoModal, setOpenInfoModal] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
   const [openSaveModal, isOpenSaveModal] = useState(false);
 
@@ -36,59 +40,73 @@ function Generator() {
   let shuffledLower;
   let newUpper;
   let newLower;
-  let shuffledFiltered;
-  let shuffledFilteredUpper;
-  let shuffledFilteredLower;
-
-  console.log(workout);
 
   useEffect(() => {
     setTimeout(() => setIsVisible(true), 150);
   }, [workout]);
 
-  const generateWorkout = () => {
+  const generateWorkout = async () => {
     setIsVisible(false);
-    handleSets();
-    handleReps();
-    shuffled = [...filteredLower].sort(() => Math.random() - 0.5);
+
+    switch (exerciseAmount) {
+      case 2:
+        setSets(2);
+        break;
+      case 3:
+        setSets(3);
+        break;
+      case 5:
+        setSets(4);
+        break;
+      default:
+        break;
+    }
 
     switch (workoutType) {
-      case "Lower":
-        shuffled = [...filteredLower].sort(() => Math.random() - 0.5);
-        shuffledFiltered = shuffled.filter(
-          (exer) => exer[`is${goal}`] === true
-        );
-        newArr = shuffledFiltered.slice(0, exerciseAmount);
+      case "Full":
+        if (exerciseTime) shuffled = EXERCISES.sort(() => Math.random() - 0.5);
+        newArr = shuffled.slice(0, exerciseAmount);
         setWorkout(newArr);
         break;
       case "Upper":
-        shuffled = [...filteredUpper].sort(() => Math.random() - 0.5);
-        shuffledFiltered = shuffled.filter(
-          (exer) => exer[`is${goal}`] === true
-        );
-        newArr = shuffledFiltered.slice(0, exerciseAmount);
-        setWorkout(newArr);
+        shuffledUpper = filteredUpper.sort(() => Math.random() - 0.5);
+        newUpper = shuffledUpper.slice(0, exerciseAmount);
+        setWorkout(newUpper);
+        console.log(newUpper);
         break;
-      case "Full":
-        shuffledUpper = [...filteredUpper].sort(() => Math.random() - 0.5);
-        shuffledLower = [...filteredLower].sort(() => Math.random() - 0.5);
-        shuffledFilteredUpper = shuffledUpper.filter(
-          (exer) => exer[`is${goal}`] === true
-        );
-        shuffledFilteredLower = shuffledLower.filter(
-          (exer) => exer[`is${goal}`] === true
-        );
-        newUpper = shuffledFilteredUpper.slice(0, exerciseAmount / 2);
-        newLower = shuffledFilteredLower.slice(0, exerciseAmount / 2 + 1);
-        newArr = newUpper.concat(newLower);
-        setWorkout(newArr);
+      case "Lower":
+        shuffledLower = filteredLower.sort(() => Math.random() - 0.5);
+        newLower = shuffledLower.slice(0, exerciseAmount);
+        setWorkout(newLower);
         break;
       default:
         break;
     }
   };
 
-  const handleReps = () => {
+  const handleTypeClick = (type) => {
+    setWorkoutType(type);
+  };
+
+  const handleTimeClick = (amount) => {
+    setExerciseTime(amount);
+    switch (amount) {
+      case 5:
+        setExerciseAmount(2);
+        break;
+      case 10:
+        setExerciseAmount(3);
+        break;
+      case 20:
+        setExerciseAmount(5);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleGoalClick = (goal) => {
+    setGoal(goal);
     switch (goal) {
       case "Power":
         setReps([1, 5]);
@@ -104,32 +122,17 @@ function Generator() {
     }
   };
 
-  const handleOpenHowTo = () => {
-    setOpenHowTo(!openHowTo);
-  };
-
-  const handleSets = () => {
-    setSets(10 / exerciseAmount);
-  };
-
-  const handleTypeClick = (type) => {
-    setWorkoutType(type);
-  };
-
-  const handleAmountClick = (amount) => {
-    setExerciseAmount(amount);
-  };
-
-  const handleGoalClick = (goal) => {
-    setGoal(goal);
-  };
-
-  const handleSaveWorkout = () => {
-    console.log("save workout");
+  const handleOpenInfoModal = () => {
+    setOpenInfoModal(!openInfoModal);
   };
 
   return (
-    <Box sx={{ textAlign: "center", margin: "auto" }}>
+    <Box
+      sx={{
+        textAlign: "center",
+        margin: "auto",
+      }}
+    >
       <InfoIcon
         sx={{
           fontSize: { xs: "2rem", lg: "2.5rem" },
@@ -138,43 +141,71 @@ function Generator() {
           top: "3.5rem",
           cursor: "pointer",
         }}
-        onClick={() => handleOpenHowTo()}
+        onClick={() => handleOpenInfoModal()}
       />
-      <ExerciseSelector
-        handleTypeClick={handleTypeClick}
-        handleAmountClick={handleAmountClick}
-        handleGoalClick={handleGoalClick}
-        workoutType={workoutType}
-        exerciseAmount={exerciseAmount}
-        goal={goal}
-      />
-      <GenerateBtn
-        generateWorkout={generateWorkout}
-        workoutType={workoutType}
-        exerciseAmount={exerciseAmount}
-        goal={goal}
-      />
-      <Workout
-        workout={workout}
-        sets={sets}
-        reps={reps}
-        repsArray={repsArray}
-        isVisible={isVisible}
-        setIsVisible={setIsVisible}
-      />
-      {workout && (
-        <Button onClick={() => isOpenSaveModal(true)}>Save Workout</Button>
-      )}
+      <Box
+        sx={{
+          position: "absolute",
+          top: { xs: "10%", sm: "20%" },
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        <ExerciseSelector
+          handleTypeClick={handleTypeClick}
+          handleTimeClick={handleTimeClick}
+          handleGoalClick={handleGoalClick}
+          workoutType={workoutType}
+          exerciseTime={exerciseTime}
+          goal={goal}
+        />
+        <GenerateBtn
+          generateWorkout={generateWorkout}
+          workoutType={workoutType}
+          exerciseAmount={exerciseTime}
+          goal={goal}
+        />
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "40%",
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: { xs: "100%", md: "80%", lg: "60%" },
+        }}
+      >
+        <Workout
+          workout={workout}
+          reps={reps}
+          sets={sets}
+          repsArray={repsArray}
+          isVisible={isVisible}
+          setIsVisible={setIsVisible}
+        />
+        {user && workout && (
+          <Button onClick={() => isOpenSaveModal(true)}>Save Workout</Button>
+        )}
+        {!user && workout && (
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            Log in to save this workout
+          </Link>
+        )}
+      </Box>
+
       <DescriptionModal open={open} setOpen={setOpen} />
       <SaveWorkoutModal
         isOpen={openSaveModal}
         setIsOpen={isOpenSaveModal}
         workout={workout}
         workoutType={workoutType}
-        exerciseAmount={exerciseAmount}
+        exerciseAmount={exerciseTime}
         goal={goal}
       />
-      <HowToModal openHowTo={openHowTo} handleOpenHowTo={handleOpenHowTo} />
+      <HowToModal
+        openInfoModal={openInfoModal}
+        handleOpenInfoModal={handleOpenInfoModal}
+      />
     </Box>
   );
 }
