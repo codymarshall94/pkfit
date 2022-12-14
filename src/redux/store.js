d92dbee081ed2afd62ab5c013731ace9ec32c695
Cody Marshall <codymstuntsx@gmail.com>
2022-12-13 16:42:14 -0800

    added selectedworkoutreducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import skillReducer from "./skills";
 import modalReducer from "./modal";
 import userReducer from "./reducers/userSlice";
+import selectedWorkoutReducer from "./reducers/selectedWorkoutSlice";
 
 export const store = configureStore({
   reducer: {
     skills: skillReducer,
     modal: modalReducer,
     user: userReducer,
+    selectedWorkout: selectedWorkoutReducer,
   },
 })
