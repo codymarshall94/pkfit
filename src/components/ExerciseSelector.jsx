d8eadc2a0e73eddc8b39ab751f473938446b867d
Cody Marshall <codymstuntsx@gmail.com>
2022-12-16 12:40:20 -0800

    created variables to map over for each selector

--- src/components/ExerciseSelector.jsx
+++ src/components/ExerciseSelector.jsx
@@ -1,5 +1,9 @@
-import { Button, ButtonGroup, Box } from "@mui/material";
+import { Button, ButtonGroup, Box, Typography } from "@mui/material";
 import React from "react";
+
+const typeOptions = ["Full", "Upper", "Lower"];
+const timeOptions = [5, 10, 20];
+const goalOptions = ["Power", "Strength", "Conditioning"];
 
 function TargetArea({
   handleTypeClick,
@@ -12,81 +16,54 @@ function TargetArea({
   return (
     <Box>
       <Box>
-        <h3>Workout Type</h3>
+        <Typography variant="h3" sx={{ margin: ".5rem 0" }}>
+          Target Area
+        </Typography>
         <ButtonGroup>
-          <Button
-            color="secondary"
-            variant={workoutType === "Full" ? "contained" : "outlined"}
-            onClick={() => handleTypeClick("Full")}
-          >
-            Full
-          </Button>
-          <Button
-            color="secondary"
-            variant={workoutType === "Upper" ? "contained" : "outlined"}
-            onClick={() => handleTypeClick("Upper")}
-          >
-            Upper
-          </Button>
-          <Button
-            color="secondary"
-            variant={workoutType === "Lower" ? "contained" : "outlined"}
-            onClick={() => handleTypeClick("Lower")}
-          >
-            Lower
-          </Button>
+          {typeOptions.map((type) => (
+            <Button
+              key={type}
+              color="secondary"
+              variant={workoutType === type ? "contained" : "outlined"}
+              onClick={() => handleTypeClick(type)}
+            >
+              {type}
+            </Button>
+          ))}
         </ButtonGroup>
       </Box>
       <Box>
-        <h3>Time&#40;min&#41;</h3>
+        <Typography variant="h3" sx={{ margin: ".5rem 0" }}>
+          Time (min)
+        </Typography>
         <ButtonGroup>
-          <Button
-            color="secondary"
-            variant={exerciseTime === 5 ? "contained" : "outlined"}
-            onClick={() => handleTimeClick(5)}
-          >
-            5
-          </Button>
-          <Button
-            color="secondary"
-            variant={exerciseTime === 10 ? "contained" : "outlined"}
-            onClick={() => handleTimeClick(10)}
-          >
-            10
-          </Button>
-          <Button
-            color="secondary"
-            variant={exerciseTime === 20 ? "contained" : "outlined"}
-            onClick={() => handleTimeClick(20)}
-          >
-            20
-          </Button>
+          {timeOptions.map((time) => (
+            <Button
+              key={time}
+              color="secondary"
+              variant={exerciseTime === time ? "contained" : "outlined"}
+              onClick={() => handleTimeClick(time)}
+            >
+              {time}
+            </Button>
+          ))}
         </ButtonGroup>
       </Box>
       <Box>
-        <h3>Workout Style</h3>
+        <Typography variant="h3" sx={{ margin: ".5rem 0" }}>
+          Goal
+        </Typography>
         <ButtonGroup>
-          <Button
-            color="secondary"
-            variant={goal === "Power" ? "contained" : "outlined"}
-            onClick={() => handleGoalClick("Power")}
-          >
-            Power
-          </Button>
-          <Button
-            color="secondary"
-            variant={goal === "Strength" ? "contained" : "outlined"}
-            onClick={() => handleGoalClick("Strength")}
-          >
-            Strength
-          </Button>
-          <Button
-            color="secondary"
-            variant={goal === "Conditioning" ? "contained" : "outlined"}
-            onClick={() => handleGoalClick("Conditioning")}
-          >
-            Conditioning
-          </Button>
+          {goalOptions.map((goalType) => (
+            <Button
+              key={goalType}
+              color="secondary"
+              variant={goal === goalType ? "contained" : "outlined"}
+              onClick={() => handleGoalClick(goalType)}
+            >
+              {goalType}
+            </Button>
+          ))}
         </ButtonGroup>
       </Box>
     </Box>
