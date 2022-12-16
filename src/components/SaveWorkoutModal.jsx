9755c65d19c7a6763457176ff22f89353dd1bf8d
Cody Marshall <codymstuntsx@gmail.com>
2022-12-16 12:41:46 -0800

    refactored code to use MUI comps

--- src/components/SaveWorkoutModal.jsx
+++ src/components/SaveWorkoutModal.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import { addDoc, serverTimestamp } from "firebase/firestore";
 import { colRef } from "../firebase-config";
 import { useSelector } from "react-redux";
+import TextField from "@mui/material/TextField";
 
 function SaveWorkoutModal({
   isOpen,
@@ -39,14 +41,14 @@ function SaveWorkoutModal({
           transform: "translate(-50%, -50%)",
           height: "auto",
           width: "auto",
-          minWidth: { xs: "90%", lg: "10%" },
+          minWidth: { xs: "90%", sm: "50%", md: "40%", xl: "30%" },
           maxWidth: { xs: "90%", lg: "30%" },
           maxHeight: "80vh",
+          padding: "3rem 0",
           overflow: "hidden",
           backgroundColor: "white",
           borderRadius: ".25rem",
           boxShadow: 24,
-          padding: "1rem",
         }}
       >
         <Box
@@ -56,17 +58,46 @@ function SaveWorkoutModal({
             flexDirection: "column",
           }}
         >
-          <h1>Name this workout</h1>
-          <input
+          <Typography
+            variant="h1"
+            sx={{
+              textAlign: "center",
+              fontSize: "1.5rem",
+              marginBottom: "1rem",
+            }}
+          >
+            Workout Name
+          </Typography>
+          <TextField
             value={workoutName}
+            sx={{ width: "75%", margin: "auto" }}
             onChange={(e) => setWorkoutName(e.target.value)}
             type="text"
-            placeholder={`${exerciseTime} ${workoutType} for ${goal}`}
+            placeholder={`${exerciseTime}s ${workoutType} for ${goal}`}
+            inputProps={{ maxLength: 30 }}
             required
           />
-          <Box sx={{ display: "flex", justifyContent: "center" }}>
-            <Button onClick={() => handleSave()}>Save</Button>
-            <Button onClick={() => setIsOpen(false)}>Cancel</Button>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "1rem",
+            }}
+          >
+            <Button
+              sx={{ marginX: ".5rem" }}
+              variant="contained"
+              onClick={() => handleSave()}
+            >
+              Save
+            </Button>
+            <Button
+              sx={{ marginX: ".5rem" }}
+              variant="outlined"
+              onClick={() => setIsOpen(false)}
+            >
+              Cancel
+            </Button>
           </Box>
         </Box>
       </Box>
