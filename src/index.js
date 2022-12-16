ff654c2a3516d4039bb187af23e0df119101fb21
Cody Marshall <codymstuntsx@gmail.com>
2022-12-16 12:40:31 -0800

    removed theme variable

--- src/index.js
+++ src/index.js
@@ -5,26 +5,8 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-const theme = createTheme({
-  palette: {
-    type: "light",
-    primary: {
-      main: "#ee6c4d",
-      light: "#F18970",
-      dark: "#A64B35",
-    },
-    secondary: {
-      main: '#6A8D73',
-      light: '#87A38F',
-      dark: '#4A6250',
-    },
-    success: {
-      main: "#4caf50",
-    },
-  },
-});
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
