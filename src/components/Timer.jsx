febe3ebbbb48c4ce1c6dae08ce2192828becbb7d
Cody Marshall <codymstuntsx@gmail.com>
2022-12-16 16:56:48 -0800

    added sx classes

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -114,7 +114,18 @@ function Timer() {
           variant="contained"
           className="btn-circle"
           onClick={() => resetTimer()}
-          sx={{ backgroundColor: "primary.main" }}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "primary.main",
+            borderRadius: "50%",
+            width: "4rem",
+            height: "4rem",
+            margin: "0 1rem",
+            transition: "all 500ms",
+            userSelect: "none",
+          }}
         >
           <RestartAltIcon />
         </Button>
