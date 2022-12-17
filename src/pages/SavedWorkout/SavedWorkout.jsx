0ee3977dff26beb087363da01f426a71ec8b9a0e
Cody Marshall <codymstuntsx@gmail.com>
2022-12-16 16:50:33 -0800

    wrapped everything in a box element

--- src/pages/SavedWorkout/SavedWorkout.jsx
+++ src/pages/SavedWorkout/SavedWorkout.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
-import Box from '@mui/material/Box'
-import Workout from '../../components/Workout'
-import { useSelector } from 'react-redux'
-import DescriptionModal from '../../components/DescriptionModal'
-import Timer from '../../components/Timer'
+import React from "react";
+import Box from "@mui/material/Box";
+import Workout from "../../components/Workout";
+import { useSelector } from "react-redux";
+import DescriptionModal from "../../components/DescriptionModal";
+import Timer from "../../components/Timer";
+import Typography from "@mui/material/Typography";
 
 function SavedWorkout() {
-  const workout = useSelector((state) => state.selectedWorkout.selectedWorkout)
+  const workout = useSelector((state) => state.selectedWorkout.selectedWorkout);
 
   return (
-    <Box>
-        <h1>{workout.name}</h1>
-        <Workout workout={workout.exercises} isVisible={true}/>
-        <DescriptionModal />
-        <Timer />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        marginTop: "5rem",
+        height: "100vh",
+      }}
+    >
+      <Typography variant="h1" sx={{margin: {xs: "1rem", sm: "2rem 0"}}}>{workout.name}</Typography>
+      <Box sx={{
+        width: {xs: "100%", md: "50%"},
+      }}>
+        <Workout workout={workout.exercises} isVisible={true} />
+      </Box>
+
+      <DescriptionModal />
+      <Timer />
     </Box>
-  )
+  );
 }
 
-export default SavedWorkout
+export default SavedWorkout;
