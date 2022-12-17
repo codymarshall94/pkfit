88eaccbf2fc5ab30c602a95f121fc317c868b850
Cody Marshall <codymstuntsx@gmail.com>
2022-12-16 16:03:07 -0800

    formatted

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -19,15 +19,15 @@ function Skills() {
   const dispatch = useDispatch();
 
   return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: { sm: "0", lg: "16rem" },
-          marginBottom: { sm: "0", lg: "-24rem" },
-        }}
-      >
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
+      <Box>
         <Typography
           variant="h2"
           sx={{
@@ -37,45 +37,56 @@ function Skills() {
         >
           Exercises for all fundamental skills to get you moving
         </Typography>
+        <Grid
+          container
+          spacing={1}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            padding: "1rem",
+            height: { xs: "60%", lg: "100%" },
+            alignItems: "center",
+          }}
+        >
+          {skills.map((skill, index) => (
+            <Grid item xs={6} md={4} lg={2} key={index}>
+              <Link to={`/exercises`}>
+                <Box
+                  className="skill-card"
+                  onClick={() => dispatch(changeSkill(skill.name))}
+                  sx={{
+                    position: "relative",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textDecoration: "none",
+                    height: {
+                      xs: "8rem",
+                      sm: "10rem",
+                      md: "12rem",
+                      lg: "14rem",
+                    },
+                  }}
+                >
+                  <img
+                    className="skill-category-image"
+                    src={skill.image}
+                    alt=""
+                  />
+                  <Typography
+                    variant="h3"
+                    className="skill-card-text"
+                    sx={{ position: "absolute" }}
+                  >
+                    {skill.name}
+                  </Typography>
+                </Box>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
       </Box>
-      <Grid
-        container
-        spacing={1}
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          padding: "1rem",
-          height: { xs: "60%", lg: "100%" },
-          alignItems: "center",
-        }}
-      >
-        {skills.map((skill, index) => (
-          <Grid item xs={6} md={4} lg={2} key={index}>
-            <Link to={`/exercises`}>
-              <Box
-                className="skill-card"
-                onClick={() => dispatch(changeSkill(skill.name))}
-                sx={{
-                  position: "relative",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  textDecoration: "none",
-                  height: { xs: "8rem", sm: "10rem", md: "12rem", lg: "14rem" },
-                }}
-              >
-                <img
-                  className="skill-category-image"
-                  src={skill.image}
-                  alt=""
-                />
-                <span className="skill-category-text">{skill.name}</span>
-              </Box>
-            </Link>
-          </Grid>
-        ))}
-      </Grid>
-    </>
+    </Box>
   );
 }
 
