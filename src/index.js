a4dbc09aa1bf85aa524024cbc8e265b5ffe487fe
Cody Marshall <codymstuntsx@gmail.com>
2023-03-01 16:53:32 -0800

    theme provider added to js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,13 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ThemeProvider theme={theme}>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </ThemeProvider>
 );
 
