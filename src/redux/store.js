9069d078dedeb9154b51dd3d81e7f0d68f3476a7
Cody Marshall <codymstuntsx@gmail.com>
2023-03-02 19:29:58 -0800

    removed modal and user state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import skillReducer from "./reducers/skillsSlice";
-import modalReducer from './reducers/modalSlice';
-import userReducer from "./reducers/userSlice";
 import selectedWorkoutReducer from "./reducers/selectedWorkoutSlice";
 
 export const store = configureStore({
   reducer: {
     skills: skillReducer,
-    modal: modalReducer,
-    user: userReducer,
     selectedWorkout: selectedWorkoutReducer,
   },
 })
