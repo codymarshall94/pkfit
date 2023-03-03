c917cba37980dd70cb1d543ad21b93e0e3f7cbbe
Cody Marshall <codymstuntsx@gmail.com>
2023-03-02 19:32:06 -0800

    protected route component

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const ProtectedRoute = () => {
+  const { currentUser, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return currentUser ? <Outlet /> : <Navigate to="/" />;
+};
+
+
+export default ProtectedRoute;
