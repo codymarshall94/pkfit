c6be1250bb2508dabfc1b7003ebb9d028c240cc4
Cody Marshall <codymstuntsx@gmail.com>
2023-03-02 19:30:36 -0800

    changed the color to secondary

--- src/components/GenerateBtn.jsx
+++ src/components/GenerateBtn.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
-function GenerateBtn({ generateWorkout, workoutInfo }) {
+const GenerateBtn = ({ workoutInfo, generateWorkout }) => {
   const { workoutType, exerciseTime, goal } = workoutInfo;
   return (
     <Box sx={{ margin: "2rem" }}>
       <Button
         variant="contained"
-        color="primary"
+        color="secondary"
         onClick={() => generateWorkout()}
         disabled={workoutType && exerciseTime && goal ? false : true}
       >
