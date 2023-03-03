939478e88452d816326bf3483cce720dae5de6ad
Cody Marshall <codymstuntsx@gmail.com>
2023-03-02 19:27:55 -0800

    imported auth context, added protected route

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
 import React from "react";
 import Home from "./pages/Home/Home";
-import Header from "./components/Navbar";
+import Header from "./pages/Global/Navbar";
 import Generator from "./pages/Generator/Generator";
 import Skill from "./pages/Skill/Skill";
 import Skills from "./pages/Skills/Skills";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import { Routes, Route } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
-import { useDispatch } from "react-redux";
-import { auth } from "./firebase-config";
-import { setUser } from "./redux/reducers/userSlice";
 import SavedWorkouts from "./pages/SavedWorkouts/SavedWorkouts";
 import SavedWorkout from "./pages/SavedWorkout/SavedWorkout";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-
+import { AuthContextProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const [theme, colorMode] = useMode();
-  const dispatch = useDispatch();
-
-  onAuthStateChanged(auth, (currentUser) => {
-    if (currentUser) {
-      dispatch(setUser({ email: currentUser.email, uid: currentUser.uid }));
-    } else {
-      dispatch(setUser(null));
-    }
-  });
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="App">
-          <Header />
-          <div className="content" style={{ marginTop: "3rem" }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/generator" element={<Generator />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/skills/:id" element={<Skill />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/saved-workouts" element={<SavedWorkouts />} />
-            <Route path="/saved-workouts/:id" element={<SavedWorkout />} />
-            <Route path="*" element={<Home />} />
-          </Routes>
+        <AuthContextProvider>
+          <div className="App">
+            <Header />
+            <div className="content" style={{ marginTop: "3rem" }}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/generator" element={<Generator />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/skills/:id" element={<Skill />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route element={<ProtectedRoute />}>
+                  <Route path="/saved-workouts" element={<SavedWorkouts />} />
+                  <Route
+                    path="/saved-workouts/:id"
+                    element={<SavedWorkout />}
+                  />
+                </Route>
+                <Route path="*" element={<Home />} />
+              </Routes>
+            </div>
           </div>
-        </div>
+        </AuthContextProvider>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
