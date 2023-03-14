4cc388064b1f1325f6dce5a297fd5f5824953498
Cody Marshall <codymstuntsx@gmail.com>
2023-03-14 16:47:21 -0700

    updated them to removed dark move

--- src/theme.js
+++ src/theme.js
@@ -1,233 +1,70 @@
-import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
 
-// color design tokens export
-export const tokens = (mode) => ({
-  ...(mode === "dark"
-    ? {
-        primary: {
-          100: "#d0d1d5",
-          200: "#a1a4ab",
-          300: "#727681",
-          400: "#1F2A40",
-          500: "#141b2d",
-          600: "#101624",
-          700: "#0c101b",
-          800: "#080b12",
-          900: "#040509",
-        },
-        primaryOrange: {
-          100: "#fdece6",
-          200: "#fcd9cd",
-          300: "#fac6b4",
-          400: "#f9b39b",
-          500: "#f7a082",
-          600: "#c68068",
-          700: "#94604e",
-          800: "#634034",
-          900: "#31201a",
-        },
-        secondaryYellow: {
-          100: "#fcf1d6",
-          200: "#f9e2ad",
-          300: "#f7d484",
-          400: "#f4c55b",
-          500: "#f1b732",
-          600: "#c19228",
-          700: "#916e1e",
-          800: "#604914",
-          900: "#30250a",
-        },
-        pink: {
-          100: "#e5d4f9",
-          200: "#cba9f2",
-          300: "#b07eec",
-          400: "#9653e5",
-          500: "#7c28df",
-          600: "#6320b2",
-          700: "#4a1886",
-          800: "#321059",
-          900: "#19082d",
-        },
-        backgroundWhite: {
-          100: "#fffdfc",
-          200: "#fffbfa",
-          300: "#fefaf7",
-          400: "#fef8f5",
-          500: "#fef6f2",
-          600: "#cbc5c2",
-          700: "#989491",
-          800: "#666261",
-          900: "#333130",
-        },
-        grey: {
-          100: "#e0e0e0",
-          200: "#c2c2c2",
-          300: "#a3a3a3",
-          400: "#858585",
-          500: "#666666",
-          600: "#525252",
-          700: "#3d3d3d",
-          800: "#292929",
-          900: "#141414",
-        },
-      }
-    : {
-        grey: {
-          100: "#141414",
-          200: "#292929",
-          300: "#3d3d3d",
-          400: "#525252",
-          500: "#666666",
-          600: "#858585",
-          700: "#a3a3a3",
-          800: "#c2c2c2",
-          900: "#e0e0e0",
-        },
-        primary: {
-          100: "#040509",
-          200: "#080b12",
-          300: "#0c101b",
-          400: "#f2f0f0", // manually changed
-          500: "#141b2d",
-          600: "#1F2A40",
-          700: "#727681",
-          800: "#a1a4ab",
-          900: "#d0d1d5",
-        },
-        greenAccent: {
-          100: "#0f2922",
-          200: "#1e5245",
-          300: "#2e7c67",
-          400: "#3da58a",
-          500: "#4cceac",
-          600: "#70d8bd",
-          700: "#94e2cd",
-          800: "#b7ebde",
-          900: "#dbf5ee",
-        },
-        redAccent: {
-          100: "#2c100f",
-          200: "#58201e",
-          300: "#832f2c",
-          400: "#af3f3b",
-          500: "#db4f4a",
-          600: "#e2726e",
-          700: "#e99592",
-          800: "#f1b9b7",
-          900: "#f8dcdb",
-        },
-        blueAccent: {
-          100: "#151632",
-          200: "#2a2d64",
-          300: "#3e4396",
-          400: "#535ac8",
-          500: "#6870fa",
-          600: "#868dfb",
-          700: "#a4a9fc",
-          800: "#c3c6fd",
-          900: "#e1e2fe",
-        },
-      }),
-});
-
-// mui theme settings
-export const themeSettings = (mode) => {
-  const colors = tokens(mode);
-  return {
-    palette: {
-      mode: mode,
-      ...(mode === "dark"
-        ? {
-            // palette values for dark mode
-            primary: {
-              main: colors.primary[500],
-            },
-            secondary: {
-              main: colors.primaryOrange[500],
-            },
-            neutral: {
-              dark: colors.grey[700],
-              main: colors.grey[500],
-              light: colors.grey[100],
-            },
-            background: {
-              default: colors.backgroundWhite[500],
-            },
-          }
-        : {
-            // palette values for light mode
-            primary: {
-              main: colors.primaryOrange[100],
-            },
-            secondary: {
-              main: colors.greenAccent[500],
-            },
-            neutral: {
-              dark: colors.grey[700],
-              main: colors.grey[500],
-              light: colors.grey[100],
-            },
-            background: {
-              default: "#fcfcfc",
-            },
-          }),
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000",
     },
-    typography: {
+    secondary: {
+      main: "#fff",
+    },
+    red: {
+      main: "#FF0336",
+      text: "#000",
+      contrastText: "#fff",
+    },
+    text: {
+      primary: "#000",
+      secondary: "#fff",
+    },
+    background: {
+      default: "#fff",
+      black: "#000",
+      grey: "#F0F0F0",
+    },
+    redContained: {
+      main: "#FF0336",
+      contrastText: "#fff",
+    },
+    grey: {
+      main: "#575455",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    fontFamily: ["Montserrat", "sans-serif"].join(","),
+    fontSize: 12,
+    h1: {
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
+      fontSize: { xs: "2rem", sm: "3rem", lg: "3rem" },
+      fontWeight: 700,
+    },
+    h2: {
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
+      fontSize: { xs: "1.5rem", sm: "2rem", lg: "2rem" },
+      fontWeight: 600,
+    },
+    h3: {
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
+      fontSize: 24,
+      fontWeight: 500,
+    },
+    h4: {
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
+      fontSize: 20,
+      fontWeight: 500,
+    },
+    h5: {
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
+      fontSize: 16,
+    },
+    h6: {
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
+      fontSize: 14,
+    },
+    span: {
       fontFamily: ["Montserrat", "sans-serif"].join(","),
       fontSize: 12,
-      h1: {
-        fontFamily: ["Montserrat", "sans-serif"].join(","),
-        fontSize: 40,
-        fontWeight: 700,
-      },
-      h2: {
-        fontFamily: ["Montserrat", "sans-serif"].join(","),
-        fontSize: 32,
-        fontWeight: 600,
-      },
-      h3: {
-        fontFamily: ["Montserrat", "sans-serif"].join(","),
-        fontSize: 24,
-        fontWeight: 500,
-      },
-      h4: {
-        fontFamily: ["Montserrat", "sans-serif"].join(","),
-        fontSize: 20,
-        fontWeight: 500,
-      },
-      h5: {
-        fontFamily: ["Montserrat", "sans-serif"].join(","),
-        fontSize: 16,
-      },
-      h6: {
-        fontFamily: ["Montserrat", "sans-serif"].join(","),
-        fontSize: 14,
-      },
-      span: {
-        fontFamily: ["Montserrat", "sans-serif"].join(","),
-        fontSize: 12,
-      },
     },
-  };
-};
-
-// context for color mode
-export const ColorModeContext = createContext({
-  toggleColorMode: () => {},
+  },
 });
-
-export const useMode = () => {
-  const [mode, setMode] = useState("dark");
-
-  const colorMode = useMemo(
-    () => ({
-      toggleColorMode: () =>
-        setMode((prev) => (prev === "light" ? "dark" : "light")),
-    }),
-    []
-  );
-
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  return [theme, colorMode];
-};
