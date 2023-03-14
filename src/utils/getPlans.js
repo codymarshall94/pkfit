aca0d82ae319007c0ea540c38e833e12137068ed
Cody Marshall <codymstuntsx@gmail.com>
2023-03-14 16:46:56 -0700

    hook to ggrab free plans data

---
+++ src/utils/getPlans.js
@@ -0,0 +1,21 @@
+import { FREEPLANS } from "../exercises/freeplans";
+
+export const getPlans = () => {
+  try {
+    return FREEPLANS;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getPlan = (id) => {
+    const idNum = parseInt(id);
+  try {
+    console.log("getPlan", idNum);
+    const plan = FREEPLANS.find((plan) => plan.id === idNum);
+    console.log("plan", plan);
+    return plan;
+  } catch (error) {
+    console.log(error);
+  }
+};
