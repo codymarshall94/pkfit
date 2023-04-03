1848e164b158755eee1c78bf53a2ac33217e8fc4
Cody Marshall <codymstuntsx@gmail.com>
2023-04-03 16:58:26 -0700

    added in the plan component and added the filter

--- src/pages/Plans/Plans.jsx
+++ src/pages/Plans/Plans.jsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { getPlans } from "../../utils/getPlans";
 import { Grid, useTheme } from "@mui/material";
-import PlanItemBox from "./PlanItemBox";
-import PageHeader from "../../components/PageHeader";
+import PageHeader from "../../components//UI/PageHeader";
+import PlanFilter from "./PlanFilter";
+import ContentCard from "../../components/ContentCard";
 
 const Plans = () => {
+  const [plans, setPlans] = useState([]);
+  const [filteredPlans, setFilteredPlans] = useState([]);
+  const [activeFilter, setActiveFilter] = useState("all");
   const theme = useTheme();
-  const data = getPlans();
+
+  useEffect(() => {
+    fetchPlans();
+  }, []);
+
+  const fetchPlans = async () => {
+    const data = getPlans();
+    setPlans(data);
+    setFilteredPlans(data);
+  };
 
   return (
     <>
       <PageHeader title="Plans" />
-      <Grid
-        container
-        backgroundColor={theme.palette.grey[100]}
-        justifyContent="center"
-      >
-        {data.map((plan) => (
-          <PlanItemBox key={plan.id} plan={plan} />
-        ))}
-      </Grid>
+      <PlanFilter
+        plans={plans}
+        setFilteredPlans={setFilteredPlans}
+        activeFilter={activeFilter}
+        setActiveFilter={setActiveFilter}
+      />
+      <AnimatePresence>
+        <Grid
+          component={motion.div}
+          layout
+          container
+          backgroundColor={theme.palette.background.default}
+          justifyContent="center"
+        >
+          {filteredPlans.map((plan) => (
+            <ContentCard
+              key={plan.id}
+              title={plan.name}
+              subtitle={plan.type}
+              tags={plan.equipment}
+              linkText="View Plan"
+              link={`/plans/${plan.id}`}
+            />
+          ))}
+        </Grid>
+      </AnimatePresence>
     </>
   );
 };
