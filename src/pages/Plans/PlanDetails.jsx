025a2d026b866c3f289eb5e790f5205c7dc32174
Cody Marshall <codymstuntsx@gmail.com>
2023-04-03 16:56:16 -0700

    updated plan details to be reusable

--- src/pages/Plans/PlanDetails.jsx
+++ src/pages/Plans/PlanDetails.jsx
@@ -5,21 +5,25 @@ import { Box, Typography, useTheme } from "@mui/material";
 import ExerciseItem from "../../components/ExerciseItem";
 import TextUnderline from "../../components/TextUnderline";
 
-const PlanHeader = ({plan}) => {
+const PlanHeader = ({ plan }) => {
   const { name, description, skillLevel } = plan;
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
-        padding: { xs: "1rem", md: "0 0 1rem 0"},
+        padding: { xs: "1rem", md: "0 0 1rem 0" },
         width: { xs: "100%", md: "80%", lg: "60%", xl: "50%" },
         margin: "0 auto",
       }}
     >
-      <Typography variant="h1" mb=".5rem">{name}</Typography>
+      <Typography variant="h1" mb=".5rem">
+        {name}
+      </Typography>
       <TextUnderline />
-      <Typography variant="h4" fontWeight="bold" my=".5rem">{skillLevel}</Typography>
+      <Typography variant="h4" fontWeight="bold" my=".5rem">
+        {skillLevel}
+      </Typography>
       <Typography variant="p">{description}</Typography>
     </Box>
   );
@@ -40,22 +44,24 @@ const PlanDetails = () => {
   if (loading) return <div>Loading...</div>;
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        mt: "5rem",
-        color: theme.palette.text.primary,
-      }}
-    >
-      <PlanHeader plan={plan} />
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        {plan.workout.map((exercise, index) => (
-          <ExerciseItem exercise={exercise} key={index} />
-        ))}
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          mt: "5rem",
+          color: theme.palette.text.primary,
+        }}
+      >
+        <PlanHeader plan={plan} />
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          {plan.workout.map((exercise, index) => (
+            <ExerciseItem exercise={exercise} key={index} />
+          ))}
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
