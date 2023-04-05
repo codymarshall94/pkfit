74562b78aad5342cf61feb26aa7b0e5408d35adb
Cody Marshall <codymstuntsx@gmail.com>
2023-04-04 18:39:18 -0700

    updated if statements

--- src/components/routelayouts/AdminPanelRoute.jsx
+++ src/components/routelayouts/AdminPanelRoute.jsx
@@ -1,22 +1,25 @@
 import { useEffect } from "react";
 import { useNavigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import LoadingSpinner from "../LoadingSpinner";
 
 const AdminPanelRoute = () => {
   const { currentUser, loading } = useAuth();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if(loading) return;
-    if (currentUser) {
-      console.log(currentUser.uid);
-      if (currentUser.uid !== "JbEwn1D7UmRuCLGgFB5CfV6J8mK2") {
-        navigate("/login");
-      }
-    }
-  }, [currentUser, navigate]);
+  console.log(currentUser);
 
-  return <Outlet />;
+  if (loading) {
+    return <LoadingSpinner />
+  }
+
+  if (currentUser && currentUser.uid !== "JbEwn1D7UmRuCLGgFB5CfV6J8mK2") {
+    navigate("/");
+  } else if (!currentUser) {
+    navigate("/");
+  } else {
+    return <Outlet />;
+  }
 };
 
 export default AdminPanelRoute;
