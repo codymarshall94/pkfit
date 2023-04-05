17ceb18b0b6f73d20ae7cd180a9a17541f06d8e7
Cody Marshall <codymstuntsx@gmail.com>
2023-04-05 10:08:57 -0700

    moved logic up into higher comp

--- src/pages/Skill/Skill.jsx
+++ src/pages/Skill/Skill.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SkillList from "./SkillList";
 import SkillHeader from "./SkillHeader";
 import { Box, useTheme } from "@mui/material";
+import SearchBar from "../../components/SearchBar";
+import { EXERCISES } from "../../data/exercises";
+import { useParams } from "react-router-dom";
 
-function Skill() {
+const Skill = () => {
   const theme = useTheme();
+  const params = useParams();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [exerciseData, setExerciseData] = useState(EXERCISES);
+  const [filteredExercises, setFilteredExercises] = useState([]);
+  const [selectedExercise, setSelectedExercise] = useState({});
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (params.id === "All") return setFilteredExercises(EXERCISES);
+    const filterExercises = EXERCISES.sort((a, b) => {
+      //sorting alphabetically before filtering out the category
+      return a.name === b.name ? 0 : a.name < b.name ? -1 : 1;
+    }).filter((exer) => exer.category.includes(params.id));
+    setExerciseData(filterExercises);
+  }, [params]);
+
+  useEffect(() => {
+    if (searchQuery === "") return setFilteredExercises(exerciseData);
+    const filteredData = exerciseData.filter((exer) =>
+      exer.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredExercises(filteredData);
+  }, [searchQuery, exerciseData]);
+
+  const handleSelect = (exer) => {
+    setSelectedExercise(exer);
+    setOpen(true);
+  };
+
   return (
     <Box
       sx={{
@@ -15,10 +47,19 @@ function Skill() {
         color: theme.palette.text.primary,
       }}
     >
-      <SkillHeader />
-      <SkillList />
+      <SkillHeader skill={params.id}/>
+      <Box sx={{ display: "flex", justifyContent: "center", marginBottom: "2rem" }}>
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      </Box>
+      <SkillList
+        selectedExercise={selectedExercise}
+        setOpen={setOpen}
+        open={open}
+        filteredExercises={filteredExercises}
+        handleSelect={handleSelect}
+      />
     </Box>
   );
-}
+};
 
 export default Skill;
