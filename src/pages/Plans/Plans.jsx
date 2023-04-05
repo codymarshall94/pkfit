927df9a4e048f408c5e31eeb3e573d102611fd9a
Cody Marshall <codymstuntsx@gmail.com>
2023-04-04 18:40:51 -0700

    loading state with skeleton

--- src/pages/Plans/Plans.jsx
+++ src/pages/Plans/Plans.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { getPlans } from "../../utils/getPlans";
-import { Grid, useTheme } from "@mui/material";
+import { Grid, Skeleton, useTheme } from "@mui/material";
 import PageHeader from "../../components//UI/PageHeader";
 import PlanFilter from "./PlanFilter";
 import ContentCard from "../../components/ContentCard";
@@ -10,9 +10,11 @@ const Plans = () => {
   const [plans, setPlans] = useState([]);
   const [filteredPlans, setFilteredPlans] = useState([]);
   const [activeFilter, setActiveFilter] = useState("all");
+  const [loading, setLoading] = useState(true);
   const theme = useTheme();
 
   useEffect(() => {
+    setLoading(true);
     fetchPlans();
   }, []);
 
@@ -20,6 +22,7 @@ const Plans = () => {
     const data = getPlans();
     setPlans(data);
     setFilteredPlans(data);
+    setLoading(false);
   };
 
   return (
@@ -39,16 +42,29 @@ const Plans = () => {
           backgroundColor={theme.palette.background.default}
           justifyContent="center"
         >
-          {filteredPlans.map((plan) => (
-            <ContentCard
-              key={plan.id}
-              title={plan.name}
-              subtitle={plan.type}
-              tags={plan.equipment}
-              linkText="View Plan"
-              link={`/plans/${plan.id}`}
-            />
-          ))}
+          {filteredPlans.map((plan) =>
+            loading ? (
+              <Skeleton variant="rounded" sx={{ margin: ".5rem" }}>
+                <ContentCard
+                  key={plan.id}
+                  title={plan.name}
+                  subtitle={plan.type}
+                  tags={plan.equipment}
+                  linkText="View Plan"
+                  link={`/plans/${plan.id}`}
+                />
+              </Skeleton>
+            ) : (
+              <ContentCard
+                key={plan.id}
+                title={plan.name}
+                subtitle={plan.type}
+                tags={plan.equipment}
+                linkText="View Plan"
+                link={`/plans/${plan.id}`}
+              />
+            )
+          )}
         </Grid>
       </AnimatePresence>
     </>
