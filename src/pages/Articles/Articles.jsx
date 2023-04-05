b1a4a736b83987fc83b52c4e318781ac80c08cd5
Cody Marshall <codymstuntsx@gmail.com>
2023-04-04 18:40:40 -0700

    added a skeleton for loading cards

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -3,18 +3,21 @@ import { getArticles } from "../../utils/getArticles";
 import PageHeader from "../../components/UI/PageHeader";
 import ContentCard from "../../components/ContentCard";
 import { AnimatePresence, motion } from "framer-motion";
-import { Grid, useTheme } from "@mui/material";
+import { Grid, useTheme, Skeleton } from "@mui/material";
 
 const Articles = () => {
   const theme = useTheme();
+  const [loading, setLoading] = useState(true);
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
+    setLoading(true);
     getArticles().then((data) => {
       setArticles(data);
     });
+    setLoading(false);
   }, []);
-  console.log(articles);
+
   return (
     <>
       <PageHeader title="Articles" />
@@ -26,16 +29,34 @@ const Articles = () => {
           backgroundColor={theme.palette.background.default}
           justifyContent="center"
         >
-          {articles.map((article) => (
-            <ContentCard
-              key={article.id}
-              title={article.title}
-              subtitle={article.type}
-              tags={article.tags}
-              linkText="Read More"
-              link={`/articles/${article.id}`}
-            />
-          ))}
+          {articles.map((article) =>
+            loading ? (
+              <Skeleton
+                variant="rounded"
+                sx={{
+                  margin: ".5rem",
+                }}
+              >
+                <ContentCard
+                  key={article.id}
+                  title={article.title}
+                  subtitle={article.type}
+                  tags={article.tags}
+                  linkText="Read More"
+                  link={`/articles/${article.id}`}
+                />
+              </Skeleton>
+            ) : (
+              <ContentCard
+                key={article.id}
+                title={article.title}
+                subtitle={article.type}
+                tags={article.tags}
+                linkText="Read More"
+                link={`/articles/${article.id}`}
+              />
+            )
+          )}
         </Grid>
       </AnimatePresence>
     </>
