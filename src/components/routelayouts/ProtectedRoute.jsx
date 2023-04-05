8ba25e10239cc7050766372613c8ea701b2445c1
Cody Marshall <codymstuntsx@gmail.com>
2023-04-04 18:39:39 -0700

    updated protected route with loading spinner

--- src/components/routelayouts/ProtectedRoute.jsx
+++ src/components/routelayouts/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import LoadingSpinner from "../LoadingSpinner";
 
 const ProtectedRoute = () => {
   const { currentUser, loading } = useAuth();
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <LoadingSpinner />;
   }
 
   return currentUser ? <Outlet /> : <Navigate to="/" />;
