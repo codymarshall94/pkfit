62c3bc305a817990f758e15628065e94d7c7f389
Cody Marshall <codymstuntsx@gmail.com>
2023-04-06 13:03:37 -0700

    removed form and changed to end adornment

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,23 +3,21 @@ import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = ({ setSearchQuery }) => {
   return (
-    <form>
-      <TextField
-        id="search"
-        label="Search"
-        variant="outlined"
-        onChange={(e) => setSearchQuery(e.target.value)}
-        placeholder="Search for a skill"
-        size="small"
-        inputProps={{
-          startadornment: (
-            <InputAdornment position="end">
-              <SearchIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-    </form>
+    <TextField
+      id="search"
+      label="Search"
+      variant="outlined"
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search for a skill"
+      size="small"
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
+    />
   );
 };
 
