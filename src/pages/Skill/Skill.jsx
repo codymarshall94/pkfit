b52d4c45bb47b1e9466575f86f9328363e860fa9
Cody Marshall <codymstuntsx@gmail.com>
2023-04-06 21:04:21 -0700

    passed in all exercises

--- src/pages/Skill/Skill.jsx
+++ src/pages/Skill/Skill.jsx
@@ -3,25 +3,37 @@ import SkillList from "./SkillList";
 import SkillHeader from "./SkillHeader";
 import { Box, useTheme } from "@mui/material";
 import SearchBar from "../../components/SearchBar";
-import { EXERCISES } from "../../data/exercises";
+import EXERCISESS from "../../data/exercisess";
 import { useParams } from "react-router-dom";
+import PageResults from "../../components/PageResults";
+
+const allExercises = [
+  ...EXERCISESS.weighted.lowerbody,
+  ...EXERCISESS.weighted.upperbody.push,
+  ...EXERCISESS.weighted.upperbody.pull,
+  ...EXERCISESS.bodyweight.lowerbody,
+  ...EXERCISESS.bodyweight.upperbody.push,
+  ...EXERCISESS.bodyweight.upperbody.pull,
+];
 
 const Skill = () => {
   const theme = useTheme();
   const params = useParams();
   const [searchQuery, setSearchQuery] = useState("");
-  const [exerciseData, setExerciseData] = useState(EXERCISES);
+  const [exerciseData, setExerciseData] = useState(allExercises);
   const [filteredExercises, setFilteredExercises] = useState([]);
   const [selectedExercise, setSelectedExercise] = useState({});
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    if (params.id === "All") return setFilteredExercises(EXERCISES);
-    const filterExercises = EXERCISES.sort((a, b) => {
-      //sorting alphabetically before filtering out the category
-      return a.name === b.name ? 0 : a.name < b.name ? -1 : 1;
-    }).filter((exer) => exer.category.includes(params.id));
-    setExerciseData(filterExercises);
+    if (params.id === "All") return setFilteredExercises(allExercises);
+    const filterAndSortedExercises = allExercises
+      .sort((a, b) => {
+        //sorting alphabetically before filtering out the category
+        return a.name === b.name ? 0 : a.name < b.name ? -1 : 1;
+      })
+      .filter((exer) => exer.category.includes(params.id));
+    setExerciseData(filterAndSortedExercises);
   }, [params]);
 
   useEffect(() => {
@@ -47,10 +59,13 @@ const Skill = () => {
         color: theme.palette.text.primary,
       }}
     >
-      <SkillHeader skill={params.id}/>
-      <Box sx={{ display: "flex", justifyContent: "center", marginBottom: "2rem" }}>
+      <SkillHeader skill={params.id} />
+      <Box
+        sx={{ display: "flex", justifyContent: "center", marginBottom: "2rem" }}
+      >
         <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       </Box>
+      <PageResults amount={filteredExercises.length} />
       <SkillList
         selectedExercise={selectedExercise}
         setOpen={setOpen}
