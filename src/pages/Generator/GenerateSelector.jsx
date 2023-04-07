ced3282b354b282842d6ba5d2ed25c6eaa890738
Cody Marshall <codymstuntsx@gmail.com>
2023-04-06 21:03:40 -0700

    upodated the selector to include checkboxes

--- src/pages/Generator/GenerateSelector.jsx
+++ src/pages/Generator/GenerateSelector.jsx
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { useTheme } from "@mui/material/styles";
-import { Button, ButtonGroup, Box, Typography } from "@mui/material";
+import {
+  Button,
+  ButtonGroup,
+  Box,
+  Typography,
+  Input,
+  InputLabel,
+  Checkbox,
+  FormGroup,
+  FormControlLabel,
+} from "@mui/material";
 
 const typeOptions = ["Full", "Upper", "Lower"];
 const timeOptions = [10, 20, 30, 60];
 const goalOptions = ["Power", "Strength", "Conditioning"];
 
-const options = [
-  { label: "Workout Type", options: typeOptions },
-  { label: "Time (min)", options: timeOptions },
-  { label: "Goal", options: goalOptions },
+const selectorOptions = [
+  { label: "Workout Type", selections: typeOptions },
+  { label: "Time (min)", selections: timeOptions },
+  { label: "Goal", selections: goalOptions },
 ];
 
 const SelectorItem = ({ label, options, value, handleClick }) => {
@@ -24,7 +34,11 @@ const SelectorItem = ({ label, options, value, handleClick }) => {
     <Box>
       <Typography
         variant="h5"
-        sx={{ margin: ".5rem 0", color: theme.palette.text.primary, fontWeight: 600 }}
+        sx={{
+          margin: ".5rem 0",
+          color: theme.palette.text.primary,
+          fontWeight: 600,
+        }}
       >
         {label}
       </Typography>
@@ -45,39 +59,99 @@ const SelectorItem = ({ label, options, value, handleClick }) => {
   );
 };
 
-function GenerateSelector({
+const GenerateSelector = ({
   handleTypeClick,
   handleGoalClick,
   handleTimeClick,
   workoutType,
   exerciseTime,
+  weighted,
+  handleWeightedClick,
   goal,
-}) {
+  handlePushClick,
+  handlePullClick,
+  push,
+  pull,
+}) => {
+  console.log(push, pull);
+  const theme = useTheme();
   return (
-    <Box>
-      {options.map((option) => (
+    <>
+      {selectorOptions.map((option) => (
         <SelectorItem
           key={option.label}
           label={option.label}
-          options={option.options}
+          options={option.selections}
           value={
             option.label === "Workout Type"
               ? workoutType
               : option.label === "Time (min)"
               ? exerciseTime
-              : goal
+              : option.label === "Goal"
+              ? goal
+              : null
           }
           handleClick={
             option.label === "Workout Type"
               ? handleTypeClick
               : option.label === "Time (min)"
               ? handleTimeClick
-              : handleGoalClick
+              : option.label === "Goal"
+              ? handleGoalClick
+              : null
           }
         />
       ))}
-    </Box>
+      <FormGroup row sx={{ marginTop: "1rem" }}>
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="weighted"
+              id="weighted"
+              checked={weighted}
+              defaultChecked="true"
+              onChange={() => handleWeightedClick(!weighted)}
+              sx={{
+                color: theme.palette.text.primary,
+              }}
+            />
+          }
+          label="Weighted"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="push"
+              id="push"
+              checked={push}
+              disabled={workoutType !== "Upper"}
+              onChange={() => handlePushClick(!push)}
+              sx={{
+                color: theme.palette.text.primary,
+              }}
+            />
+          }
+          label="Push"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="pull"
+              id="pull"
+              checked={pull}
+              disabled={workoutType !== "Upper"}
+              defaultChecked="true"
+              onChange={() => handlePullClick(!pull)}
+              sx={{
+                color: theme.palette.text.primary,
+              }}
+            />
+          }
+          label="Pull"
+        />
+      </FormGroup>
+    </>
   );
-}
+};
 
 export default GenerateSelector;
