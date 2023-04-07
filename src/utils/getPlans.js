cfed49e40bd6016bf2726d39b20c6b00797c8793
Cody Marshall <codymstuntsx@gmail.com>
2023-04-06 21:04:55 -0700

    removed console logs

--- src/utils/getPlans.js
+++ src/utils/getPlans.js
@@ -11,9 +11,7 @@ export const getPlans = () => {
 export const getPlan = (id) => {
     const idNum = parseInt(id);
   try {
-    console.log("getPlan", idNum);
     const plan = FREEPLANS.find((plan) => plan.id === idNum);
-    console.log("plan", plan);
     return plan;
   } catch (error) {
     console.log(error);
