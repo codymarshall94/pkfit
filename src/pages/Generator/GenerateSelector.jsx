2e43a7f1f7679b6ffb4cf7102e01e00bc44b0fd3
Cody Marshall <codymstuntsx@gmail.com>
2023-04-07 17:14:38 -0700

    condensed logic into arrays

--- src/pages/Generator/GenerateSelector.jsx
+++ src/pages/Generator/GenerateSelector.jsx
@@ -1,154 +1,88 @@
-import React, { useState, useEffect } from "react";
-import { useTheme } from "@mui/material/styles";
-import {
-  Button,
-  ButtonGroup,
-  Box,
-  Typography,
-  Input,
-  InputLabel,
-  Checkbox,
-  FormGroup,
-  FormControlLabel,
-} from "@mui/material";
+import { FormGroup } from "@mui/material";
+import GeneratorCheckbox from "./GeneratorCheckbox";
+import SelectorItem from "./SelectorItem";
 
 const typeOptions = ["Full", "Upper", "Lower"];
 const timeOptions = [10, 20, 30, 60];
 const goalOptions = ["Power", "Strength", "Conditioning"];
 
-const selectorOptions = [
-  { label: "Workout Type", selections: typeOptions },
-  { label: "Time (min)", selections: timeOptions },
-  { label: "Goal", selections: goalOptions },
-];
-
-const SelectorItem = ({ label, options, value, handleClick }) => {
-  const theme = useTheme();
-  const [selected, setSelected] = useState(value);
-
-  useEffect(() => {
-    setSelected(value);
-  }, [value]);
-
-  return (
-    <Box>
-      <Typography
-        variant="h5"
-        sx={{
-          margin: ".5rem 0",
-          color: theme.palette.text.primary,
-          fontWeight: 600,
-        }}
-      >
-        {label}
-      </Typography>
-      <ButtonGroup>
-        {options.map((option) => (
-          <Button
-            key={option}
-            color="grey"
-            variant={selected === option ? "contained" : "outlined"}
-            onClick={() => handleClick(option)}
-            sx={{ borderRadius: "2rem" }}
-          >
-            {option}
-          </Button>
-        ))}
-      </ButtonGroup>
-    </Box>
-  );
-};
-
 const GenerateSelector = ({
   handleTypeClick,
   handleGoalClick,
   handleTimeClick,
-  workoutType,
-  exerciseTime,
-  weighted,
   handleWeightedClick,
-  goal,
   handlePushClick,
   handlePullClick,
+  weighted,
   push,
   pull,
+  workoutType,
+  exerciseTime,
+  goal,
 }) => {
-  console.log(push, pull);
-  const theme = useTheme();
+  const selectors = [
+    {
+      label: "Workout Type",
+      options: typeOptions,
+      value: workoutType,
+      handleClick: handleTypeClick,
+    },
+    {
+      label: "Time (min)",
+      options: timeOptions,
+      value: exerciseTime,
+      handleClick: handleTimeClick,
+    },
+    {
+      label: "Goal",
+      options: goalOptions,
+      value: goal,
+      handleClick: handleGoalClick,
+    },
+  ];
+
+  const checkBoxes = [
+    {
+      label: "Weighted",
+      value: weighted,
+      handleClick: handleWeightedClick,
+      isDisabled: false,
+    },
+    {
+      label: "Push",
+      value: push,
+      handleClick: handlePushClick,
+      isDisabled: workoutType !== "Upper",
+    },
+    {
+      label: "Pull",
+      value: pull,
+      handleClick: handlePullClick,
+      isDisabled: workoutType !== "Upper",
+    },
+  ];
+
   return (
     <>
-      {selectorOptions.map((option) => (
+      {selectors.map((selector) => (
         <SelectorItem
-          key={option.label}
-          label={option.label}
-          options={option.selections}
-          value={
-            option.label === "Workout Type"
-              ? workoutType
-              : option.label === "Time (min)"
-              ? exerciseTime
-              : option.label === "Goal"
-              ? goal
-              : null
-          }
-          handleClick={
-            option.label === "Workout Type"
-              ? handleTypeClick
-              : option.label === "Time (min)"
-              ? handleTimeClick
-              : option.label === "Goal"
-              ? handleGoalClick
-              : null
-          }
+          key={selector.label}
+          label={selector.label}
+          options={selector.options}
+          value={selector.value}
+          handleClick={selector.handleClick}
         />
       ))}
       <FormGroup row sx={{ marginTop: "1rem" }}>
-        <FormControlLabel
-          control={
-            <Checkbox
-              name="weighted"
-              id="weighted"
-              checked={weighted}
-              defaultChecked="true"
-              onChange={() => handleWeightedClick(!weighted)}
-              sx={{
-                color: theme.palette.text.primary,
-              }}
-            />
-          }
-          label="Weighted"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              name="push"
-              id="push"
-              checked={push}
-              disabled={workoutType !== "Upper"}
-              onChange={() => handlePushClick(!push)}
-              sx={{
-                color: theme.palette.text.primary,
-              }}
-            />
-          }
-          label="Push"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              name="pull"
-              id="pull"
-              checked={pull}
-              disabled={workoutType !== "Upper"}
-              defaultChecked="true"
-              onChange={() => handlePullClick(!pull)}
-              sx={{
-                color: theme.palette.text.primary,
-              }}
-            />
-          }
-          label="Pull"
-        />
+        {checkBoxes.map((checkBox) => (
+          <GeneratorCheckbox
+            key={checkBox.label}
+            label={checkBox.label}
+            value={checkBox.value}
+            handleClick={checkBox.handleClick}
+            isDisabled={checkBox.isDisabled}
+          />
+        ))}
       </FormGroup>
     </>
   );
